779dbd802cbcb834f9ab61909afd777f624d0b11
hessi3700 <hessi3700@gmail.com>
2026-02-21 14:52:11 +0330

    Updates

--- backend/dist/diagrams/dto/update-diagram.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class UpdateDiagramDto {
-    title?: string;
-    description?: string;
-    customMermaidCode?: string;
-    nodes?: any;
-    edges?: any;
-    metadata?: any;
-}
--- backend/dist/tasks/tasks.seed-data.d.ts
+++
@@ -1,2 +0,0 @@
-import { CreateTaskDto } from './dto/create-task.dto';
-export declare const ALL_LEVEL_TASKS: CreateTaskDto[];
